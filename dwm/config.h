ea9c6c3d86340e8f9328f3dbf6bd2625a1cc78b7
Matthijs Kool <matthijzk@gmail.com>
2019-08-19 19:28:33 +0200

    DWM: Set white border around focused window

--- dwm/config.h
+++ dwm/config.h
@@ -8,7 +8,7 @@ static const char dmenufont[]       = "monospace:size=10";
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#bbbbbb";
-static const char selbordercolor[]  = "#005577";
+static const char selbordercolor[]  = "#F5F5F5";
 static const char selbgcolor[]      = "#005577";
 static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
