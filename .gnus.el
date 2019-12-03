d8ce77f3a58f88b1b108b115c55fc81f51ae7579
Matthijs Kool <matthijzk@gmail.com>
2019-12-03 09:02:22 +0100

    Gnus: use setq-local to set options for specific groups
    Also handle setting of options in a cond in a single lambda.

--- .gnus.el
+++ .gnus.el
@@ -4,14 +4,12 @@
 ;; Summary buffer
 (setq gnus-summary-mode-line-format "Gnus: %G [%A] %Z")
 
-(defconst my-gnus-summary-line-format gnus-summary-line-format "Backup for `gnus-summary-line-format'")
-(setq gnus-summary-mode-hook nil)
-
 (add-hook 'gnus-summary-mode-hook #'hl-line-mode)
 
 (add-hook 'gnus-summary-mode-hook
           (lambda ()
-            "Fill NOS Nieuws articles, as these are basically unfilled html."
-            (if (string-match-p "gwene.nl.nos.nosnieuws" gnus-newsgroup-name)
-                (setq gnus-treat-fill-article t)
-              (setq gnus-treat-fill-article nil))))
+            "Set custom options for specific groups."
+            (cond ((string-match-p "gwene.nl.nos.nosnieuws" gnus-newsgroup-name)
+                   (setq-local gnus-summary-line-format "%*%U%R %-20&user-date;| %S\n")
+                   ;; Fill NOS Nieuws articles, as these are basically unfilled html.
+                   (setq-local gnus-treat-fill-article t)))))
