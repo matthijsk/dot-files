8bd009505941af28106eeec713380177b9fc5b91
Matthijs Kool <matthijzk@gmail.com>
2023-04-18 13:30:46 +0200

    Add Set-ExecutionPolicy description to Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
 # Copy to ~/Documents/WindowsPowerShell
+# To enable this script, run the command "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned"
+# See https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.3 for more information
 Set-PSReadLineOption -EditMode Emacs
